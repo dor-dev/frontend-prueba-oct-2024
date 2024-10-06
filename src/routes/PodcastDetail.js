cf1e6badb2b00c7110d56a4da3ff318ce581fb2b
dor <dor.webdeveloper@gmail.com>
2024-10-06 04:53:56 +0200

    Fetch and data caching on Podcast Detail

--- src/routes/PodcastDetail.js
+++ src/routes/PodcastDetail.js
@@ -1,85 +1,136 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { usePodcastContext } from "../PodcastContext";
 
-function PodcastDetail({params}) {
-  const podcastPathId = "1";
+function PodcastDetail({setLoading}) {
+  const { podcastId } = useParams();
+
+  const [episodes, setEpisodes] = useState([]);
   
-  let updateData = true;
+  useEffect(() => {
+    const podcastData = localStorage.getItem("podcast-episodes");
 
-  let podcastEpisodes = null;
+    const lastDay = new Date() - (1000 * 60 * 60 * 24);
 
-  const storedEpisodes = localStorage.getItem("episodes");
-  const lastUpdateDate = localStorage.getItem("episodes-last-updated");
-  if (storedEpisodes) {
-    const currentPodcastEpisodes = JSON.parse(storedEpisodes)[podcastPathId];
-    if (currentPodcastEpisodes) {
-      podcastEpisodes = currentPodcastEpisodes;
-      updateData = false;
+    if (podcastData) {
+      const parsedPodcastData = JSON.parse(podcastData);
+
+      const currentPodcastData = parsedPodcastData.find(podcast => podcast.id === podcastId);
+
+      const updateIsExpired = !currentPodcastData?.lastUpdated
+      || (new Date(currentPodcastData.lastUpdated)) < lastDay;
+
+      if (!updateIsExpired && currentPodcastData?.items) {
+        setEpisodes(currentPodcastData.items);
+        return;
+      }
     }
-  }
 
-  // TODO : Refactor.
+    fetchPodcastList();
 
-  const lastDay = new Date() - (1000 * 60 * 60 * 24);
+    async function fetchPodcastList() {
 
-  const updateIsExpired = !lastUpdateDate 
-    || (new Date(JSON.parse(lastUpdateDate))) < lastDay;
+      try {
+        setLoading(true);
 
-  if (updateData === true || updateIsExpired) {
-    console.warn("MAKING A REQUEST");
-    podcastEpisodes = Array.from({length: 66}, (_, i) => {
-      return {
-        title: "Episode " + i, 
-        date: new Date() - (i * 1000000000), 
-        duration: "10:00", 
-      };
-    });
-    // TODO: Make request
-    const storageEpisodes = localStorage.getItem("episodes");
-    if (storageEpisodes) {
-      const updatedEpisodes = JSON.parse(storageEpisodes);
-      updatedEpisodes[podcastPathId] = podcastEpisodes;
-      localStorage.setItem("episodes", JSON.stringify(updatedEpisodes));
-      localStorage.setItem("episodes-last-updated", new Date().getTime());
-    } else {
-      const newStorageEpisode = {[podcastPathId]: podcastEpisodes};
-      localStorage.setItem("episodes", JSON.stringify([newStorageEpisode]));
-      localStorage.setItem("episodes-last-updated", new Date().getTime());
+        const response = await fetch(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`);
+
+        if (!response.ok) {
+          throw new Error("Couldn't fetch data");
+        }
+
+        const result = await response.json();
+
+        console.warn("RESPONSE", result);
+        
+        const formattedResultPodcasts = result["results"]
+          .filter(episode => episode["wrapperType"] !== "track")
+          .map(episode => {
+          return {
+            id: episode["trackId"], 
+            title: episode["trackName"], 
+            date: new Date(episode["releaseDate"]).getTime(), 
+            duration: episode["trackTimeMillis"], 
+            author: episode["artistName"], 
+            description: episode["description"], 
+          };
+        })
+
+        const newPodcastData = {
+          id: podcastId, 
+          items: formattedResultPodcasts, 
+          lastUpdated: new Date().getTime(), 
+        };
+
+        if (podcastData) {
+          localStorage.setItem("podcast-episodes", JSON.stringify([...JSON.parse(podcastData), newPodcastData]));
+        } else {
+          localStorage.setItem("podcast-episodes", JSON.stringify([newPodcastData]));
+        }
+
+        setEpisodes(formattedResultPodcasts);
+
+      } catch (error) {
+        throw new Error(error);
+      } finally {
+        setLoading(false);
+      }
     }
-  }
+
+  }, [podcastId, setLoading]);
 
   return (
-    <div className="flex-auto">
+    <>
       <div className="p-3 shadow-md mb-4">
-        <p className="text-lg"><b>Episodes: 66</b></p>
+        <p className="text-lg"><b>Episodes: {episodes.length}</b></p>
       </div>
-      <EpisodeList episodes={podcastEpisodes}/>
-    </div>
+      <EpisodeList episodes={episodes}/>
+    </>
   );
 }
 
 export default PodcastDetail;
 
 function EpisodeList({episodes}) {
+  
+  const podcast = usePodcastContext();
+
+  const formatDuration = (duration) => {
+    const totalSeconds = Math.floor(duration / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    const formattedTime = 
+      String(hours).padStart(2, '0') + ':' + 
+      String(minutes).padStart(2, '0') + ':' + 
+      String(seconds).padStart(2, '0');
+
+    return formattedTime;
+  }
+
   return (
     <div className="p-3 shadow-md">
-      <div className="flex font-bold border-b-2 p-1 gap-1">
-        <p className="flex-auto">Title</p>
-        <p className="basis-20">Date</p>
-        <p className="basis-24 text-right">Duration</p>
+      <div className="flex font-bold border-b-2 p-1 gap-2">
+        <p className="flex-1">Title</p>
+        <p className="basis-20 flex-none">Date</p>
+        <p className="basis-20 flex-none text-right">Duration</p>
       </div>
-      {episodes.map((episode, i) => {
-        return (
-          <div 
-            className={`flex p-1 gap-1 ${i % 2 === 0 ? "bg-zinc-50" : ""}`}
-            key={episode.title}>
-            <Link to="episode/10" className="flex-auto text-sky-500">
-              <p>{episode.title}</p>
-            </Link>
-            <p className="basis-24">{new Date(episode.date).toLocaleDateString()}</p>
-            <p className="basis-20 text-right">{episode.duration}</p>
-          </div>
-        )
-      })}
+      <ul>
+        {episodes.map((episode, i) => {
+          return (
+            <li 
+              className={`flex p-1 gap-2 ${i % 2 === 0 ? "bg-zinc-50" : ""}`}
+              key={episode.id}>
+              <Link to={"episode/" + episode.id} state={podcast} className="flex-1 text-sky-500">
+                {episode.title}
+              </Link>
+              <p className="basis-20 flex-none">{new Date(episode.date).toLocaleDateString()}</p>
+              <p className="basis-20 flex-none text-right">{formatDuration(episode.duration)}</p>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
